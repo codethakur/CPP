5c897acf62e645d6abaccd2faec2f68a20c33704
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2024-02-05 16:46:36 +0530

    act

---
+++ HelloWorld/CppFun.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+//int multiply(int a, int b)
+//{
+//	return a * b;
+//}
+
+void multiply(int a, int b)
+{
+	int result = a * b;
+	std::cout << result << std::endl;
+}
+
+int main()
+{
+	/*int reasult1 = multiply(5, 8);
+	std::cout << reasult1 << std::endl;
+
+	int reasult2 = multiply(8, 8);
+	std::cout << reasult2 << std::endl;
+
+	int reasult3 = multiply(6, 7);
+	std::cout << reasult3 << std::endl;*/
+
+
+	multiply(5, 8);
+	multiply(8, 8);
+	multiply(6, 7);
+}
