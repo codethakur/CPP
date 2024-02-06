2f12ea1e39aa1495a0f899f629c48c38323c53bd
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2024-02-06 17:12:51 +0530

    Create PureVirtual

---
+++ Virtual Functions/PureVirtual.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+class Printing {
+public:
+    virtual std::string GetName() = 0;
+};
+
+class Entity : public Printing {
+public:
+    std::string GetName() override { return "Entity"; }
+};
+
+class Player : public Entity {
+private:
+    std::string m_Name;
+public:
+    Player(const std::string& name)
+        : m_Name(name) {}
+
+    std::string GetName() override { return m_Name; }
+};
+
+int main()
+{
+    Entity* e = new Entity();
+    std::cout << "Entity name: " << e->GetName() << std::endl;
+
+    Player* p = new Player("Coder");
+    std::cout << "Player name: " << p->GetName() << std::endl;
+
+    delete e;
+    delete p;
+
+    std::cin.get();
+}
