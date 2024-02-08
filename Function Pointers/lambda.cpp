5fcee973e370a4fc883ea91f53dcf6c05bd92412
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2024-02-08 23:30:56 +0530

    lambda | C++

---
+++ Function Pointers/lambda.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+
+
+
+void ForEach(std::vector<int>&values, const std::function<void(int)>& func) // auto& func
+{
+	for (int value : values) {
+		func(value);
+	}
+}
+
+int main()
+{
+	std::vector<int> values = { 1,5,6,9 };
+
+	auto lamda = [](int value) {
+		std::cout << "Values: " << value << std::endl;
+		};
+	
+	ForEach(values, lamda);
+
+
+	std::cin.get();
+}
