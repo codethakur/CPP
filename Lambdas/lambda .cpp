5c35a101354cba075b936891a55ca8a48479559b
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2024-02-08 23:58:33 +0530

    []lambda C++

---
+++ Lambdas/lambda .cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+
+void ForEach(std::vector<int>& values, const std::function<void(int)>& func)
+{
+    for (int value : values) {
+        func(value);
+    }
+}
+
+int main()
+{
+    std::vector<int> values = { 1, 5, 6, 9 };
+
+    auto lambda = [](int value) {
+        std::cout << "Values: " << value << std::endl;
+    };
+
+    ForEach(values, lambda);
+
+    std::cin.get();
+}
