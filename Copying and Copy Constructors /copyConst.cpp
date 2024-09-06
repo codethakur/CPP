2f1f38c3e3dccffb69a85d519b7ed07db21965d2
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2024-09-06 15:43:37 +0530

    Update copyConst.cpp

--- Copying and Copy Constructors /copyConst.cpp
+++ Copying and Copy Constructors /copyConst.cpp
@@ -1,69 +1,56 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <cstring>
 
-class String {
-private:
-	char* m_data;
-	int m_size;
-public:
-	String(const char* string) {
-		m_size = strlen(string);
-		m_data = new char[m_size + 1];
-		memcpy(m_data, string, m_size);
-		m_data[m_size] = 0;
-	}
-	
-	// Copy constructor
-	String(const String& other) : m_size(other.m_size) {
-		m_data = new char[m_size + 1];
-		std::memcpy(m_data, other.m_data, m_size + 1);
-	}
-	~String() {
-		delete[] m_data;
-	}
-
-	char& operator[](unsigned int index) {
-		if (index < static_cast<unsigned int>(m_size)) {
-			return m_data[index];
-		}
-		else {
-			// Handle out-of-bounds access (you might want to throw an exception)
-			// For simplicity, I'm returning the last character in the string
-			return m_data[m_size - 1];
-		}
-	}
-
-
-	// Copy assignment operator
-	String& operator=(const String& other) {
-		if (this != &other) {
-			delete[] m_data;
-			m_size = other.m_size;
-			m_data = new char[m_size + 1];
-			std::memcpy(m_data, other.m_data, m_size + 1);
-		}
-		return *this;
-	}
-
-	friend std::ostream& operator<<(std::ostream& stream, const String& string);
-};
-	std::ostream& operator<<(std::ostream& stream, const String& string) {
-		stream << string.m_data;
-		return stream;
-	}
-
-
-
-int main() 
+class String
 {
-	String str1 = "original";
-	String str2 = str1;
-	
-	str2[0] = 'n';
+private:
+    char *m_Buffre;
+    unsigned m_size;
 
-	std::cout << str1 << std::endl;
-	std::cout << str2 << std::endl;
+public:
+    String(const char *string)
+    {
+        m_size = strlen(string);
+        m_Buffre = new char[m_size + 1]; 
+        memcpy(m_Buffre, string, m_size);
+        m_Buffre[m_size] = 0; 
+    }
 
+    // Copy Constructor
+    String(const String&other):m_size(other.m_size)
+    {
+        m_Buffre = new char[m_size+1];
+        memcpy(m_Buffre, other.m_Buffre, m_size+1);
+    }
+   
+    
+    ~String()
+    {
+        delete[] m_Buffre;
+    }
+    
+    char& operator[](unsigned int index)
+    {
+        return m_Buffre[index];
+    }
+    
+    friend std::ostream &operator<<(std::ostream &stream, const String &string);
+};
 
-	std::cin.get();
+std::ostream &operator<<(std::ostream &stream, const String &string)
+{
+    stream << string.m_Buffre;
+    return stream;
+}
+
+int main()
+{
+    String string = "Kishan";
+    String second = string;
+    second[1] = 'y';
+
+    std::cout << string << std::endl; 
+    std::cout << second << std::endl; 
+
+    return 0;
 }
